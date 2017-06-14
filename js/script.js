d31a6f878cb42ea725859924e2e5d564836dbe37
Kevin Leong <kevin.leong1@uq.net.au>
2017-06-14 14:27:22 +1000

    initial files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Smooth Scrolling On Anchor Links 
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+});
